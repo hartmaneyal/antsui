3db27c178bcdbed71d31795b26a5dde198616629
EYAL HARTMAN <EYALHAR@rf.local>
2019-05-27 08:19:46 +0300

    updated gauge

--- src/js/toolData.js
+++ src/js/toolData.js
@@ -1,9 +1,6 @@
 const electron = require('electron');
 const { remote, ipcRenderer : ipc } = electron;
 
-const LinearGauge = require('canvas-gauges').LinearGauge;
-let gauge;
-
 const emiter = require('./emiter');
 var db = require('./db');
 
@@ -17,46 +14,9 @@ window.addEventListener('DOMContentLoaded', _ => {
     id = parts[1].split('=')[1];
 
     document.getElementById('spn').innerHTML = id;
-    setupGauge();
 
     db.basicInit();
 });
-
-function setupGauge(){
-    gauge = new LinearGauge({
-        renderTo: 'batteryGauge',
-        width: 250,
-        height: 100,
-        minValue: 0,
-        maxValue: 100,
-        majorTicks: [ "0", "20", "40", "60", "80", "100" ],
-        minorTicks: 10,
-        strokeTicks: true,
-        colorPlate: "#222",
-        borderShadowWidth: 0,
-        borders: false,
-        barBeginCircle: false,
-        tickSide: "left",
-        numberSide: "left",
-        needleSide: "left",
-        needleType: "line",
-        needleWidth: 3,
-        colorNeedle: "#fff",
-        colorNeedleEnd: "#fff",
-        animationDuration: 1500,
-        animationRule: "linear",
-        animationTarget: "plate",
-        barWidth: 5,
-        ticksWidth: 50,
-        ticksWidthMinor: 15,
-        colorTitle: "#eee",
-        colorNumbers: "#eee",
-        highlights: [
-            { "from": 0, "to": 25, "color": "rgba(0,0, 255, .3)" },
-            { "from": 26, "to": 50, "color": "rgba(255, 0, 0, .3)" }
-        ],
-    }).draw();
-};
 
 emiter.on('basicInit-ready', _ => {
     console.log('DB Ready');
@@ -66,10 +26,10 @@ emiter.on('basicInit-ready', _ => {
 });
 
 var lastKey = -1;
-let batteryLevel = -1;
 emiter.on('toolData-ready', (data) => {
     console.log('Data refreshed');
     let toolTable = document.getElementById('toolTableRecords');
+    let battSpan = document.getElementById('battSpan');
     for(let i = 0; i < data.length; i++){
         const tr = document.createElement("tr");
 
@@ -93,8 +53,7 @@ emiter.on('toolData-ready', (data) => {
 
         if(data[i].key > lastKey) {
             lastKey = data[i].key;
-            batteryLevel = data[i].battery;
+            battSpan.innerHTML = data[i].battery + '%';
         }
     }
-    gauge.value = batteryLevel;
 });
